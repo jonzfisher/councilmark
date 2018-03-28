2e5da2141ca2d85685a801ac066c0777f4d163da
Jonathan Fisher <jfisher@eab.com>
2018-03-28 08:41:13 -0500

    initial gatsby commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Home</h1>
+    <p>Welcome to Councilmark Asset Management, LLC.</p>
+    <p>We'll treat ya right.</p>
+    <Link to="/page-2/">Contact us</Link>
+  </div>
+)
+
+export default IndexPage
